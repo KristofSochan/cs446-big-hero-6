bbffe13bdbe0fd5277374b55fa7acd3f2668bc1c
KristofSochan <kristof.sochan@gmail.com>
2026-02-04 11:08:24 -0500

    feat: personalize hello world message

--- app/src/main/java/ca/uwaterloo/cs446/bighero6/MainActivity.kt
+++ app/src/main/java/ca/uwaterloo/cs446/bighero6/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name, we are Big Hero 6!",
         modifier = modifier
     )
 }
